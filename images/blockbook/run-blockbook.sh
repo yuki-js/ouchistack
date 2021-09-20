fc29e4b2902f85e81084e7c9edb69078577054c3
sinekasu <fuck@you.com>
2021-09-20 18:57:42 +0000

    moancoin

--- images/blockbook/run-blockbook.sh
+++ images/blockbook/run-blockbook.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-
+cd /go/src/github.com/trezor/blockbook
 exec /go/src/github.com/trezor/blockbook/blockbook -sync -blockchaincfg /chaincfg.json -datadir /data -internal :9000 -public :8000 -log_dir /log "$@"
---
+++ images/monacoin-core/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+if [[ $NETWORK == "mainnet" ]]; then
+  CHAIN="main"
+elif [[ $NETWORK == "testnet" ]]; then
+  CHAIN="test"
+elif [[ $NETWORK == "regtest" ]]; then
+  CHAIN="regtest"
+elif [[ $NETWORK == "simnet" ]]; then
+  CHAIN="simnet"
+else
+  echo "Unknown network: $NETWORK"
+  exit 1
+fi
+
+exec /usr/bin/monacoind -conf=/monacoin.conf -chain=$CHAIN "$@"
