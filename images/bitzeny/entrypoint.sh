8a1141e69a665bd84102f1eeb28ded642ba44115
yuki-js <20838151+yuki-js@users.noreply.github.com>
2021-10-17 13:24:43 +0000

    bitzeny

--- images/bitzeny/entrypoint.sh
+++ images/bitzeny/entrypoint.sh
@@ -1,16 +1,3 @@
 #!/usr/bin/env sh
 
-if [[ $NETWORK == "mainnet" ]]; then
-  CHAIN="main"
-elif [[ $NETWORK == "testnet" ]]; then
-  CHAIN="test"
-elif [[ $NETWORK == "regtest" ]]; then
-  CHAIN="regtest"
-elif [[ $NETWORK == "simnet" ]]; then
-  CHAIN="simnet"
-else
-  echo "Unknown network: $NETWORK"
-  exit 1
-fi
-
-exec /usr/local/bin/bitzenyd -conf=/bitzeny.conf -chain=$CHAIN "$@"
+exec bitzenyd -conf=/bitzeny.conf "$@"
