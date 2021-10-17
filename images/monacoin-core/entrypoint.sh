8cbc8e33a69ae1992ebd0ba1d286176d9ee42b05
yuki-js <20838151+yuki-js@users.noreply.github.com>
2021-10-17 10:01:28 +0000

    monacoind has no param for chain

--- images/monacoin-core/entrypoint.sh
+++ images/monacoin-core/entrypoint.sh
@@ -1,16 +1,3 @@
 #!/usr/bin/env bash
 
-if [[ $NETWORK == "mainnet" ]]; then
-  CHAIN="main"
-elif [[ $NETWORK == "testnet" ]]; then
-  CHAIN="test"
-elif [[ $NETWORK == "regtest" ]]; then
-  CHAIN="regtest"
-elif [[ $NETWORK == "simnet" ]]; then
-  CHAIN="simnet"
-else
-  echo "Unknown network: $NETWORK"
-  exit 1
-fi
-
-exec monacoind -conf=/monacoin.conf -chain=$CHAIN "$@"
+exec monacoind -conf=/monacoin.conf "$@"
