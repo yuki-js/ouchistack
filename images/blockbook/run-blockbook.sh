cff5575736d8c164b5b38a8f2a6ad2e9f0641440
yuki-js <20838151+yuki-js@users.noreply.github.com>
2021-10-17 07:49:38 +0000

    add single threaded options
    by this db keeps consistency and improves tolerance for shutdown

--- images/blockbook/run-blockbook.sh
+++ images/blockbook/run-blockbook.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 cd /go/src/github.com/trezor/blockbook
 rm /data/LOCK || true
-exec /go/src/github.com/trezor/blockbook/blockbook -sync -blockchaincfg /chaincfg.json -datadir /data -internal :9000 -public :8000 -log_dir /log "$@"
+exec /go/src/github.com/trezor/blockbook/blockbook -sync -blockchaincfg /chaincfg.json -datadir /data -internal :9000 -public :8000 -log_dir /log -workers 1 "$@"
