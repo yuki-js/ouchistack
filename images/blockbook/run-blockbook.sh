92fe696391e62fbe07b808a0f6c4932e85c9fdcf
yuki-js <20838151+yuki-js@users.noreply.github.com>
2021-10-17 06:10:34 +0000

    remove /data/LOCK before launch

--- images/blockbook/run-blockbook.sh
+++ images/blockbook/run-blockbook.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 cd /go/src/github.com/trezor/blockbook
+rm /data/LOCK || true
 exec /go/src/github.com/trezor/blockbook/blockbook -sync -blockchaincfg /chaincfg.json -datadir /data -internal :9000 -public :8000 -log_dir /log "$@"
