07353a716629849bf91db98a91ea2ea55b0f7f85
yuki-js <20838151+yuki-js@users.noreply.github.com>
2022-02-25 13:40:29 +0000

    w heimdal conifng

--- images/heimdall/entrypoint.sh
+++ images/heimdall/entrypoint.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env sh
 
 if [ ! -f $HEIMDALL_DIR/config/node_key.json ]; then
-    heimdalld --home=$HEIMDALL_DIR init
+    heimdalld --home=$HEIMDALL_DIR --with-heimdall-config /heimdall-config.toml init
 fi;
 
 if [ "$1" = 'bridge' ]; then
     shift
-    exec bridge --home=$HEIMDALL_DIR "$@"
+    exec bridge --home=$HEIMDALL_DIR --with-heimdall-config /heimdall-config.toml "$@"
 fi
 
 if [ "$1" = 'heimdallcli' ]; then
     shift
-    exec heimdallcli --home=$HEIMDALL_DIR "$@"
+    exec heimdallcli --home=$HEIMDALL_DIR --with-heimdall-config /heimdall-config.toml "$@"
 fi
 
-exec heimdalld --home=$HEIMDALL_DIR "$@"
+exec heimdalld --home=$HEIMDALL_DIR --with-heimdall-config /heimdall-config.toml "$@"
