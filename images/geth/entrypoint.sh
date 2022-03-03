ccd5cfec4eb9756d9da2ac3ef22e3954f80e84e7
yuki-js <20838151+yuki-js@users.noreply.github.com>
2022-03-03 12:00:50 +0000

    commandfix

--- images/geth/entrypoint.sh
+++ images/geth/entrypoint.sh
@@ -2,4 +2,4 @@
 exec geth --datadir=/datadir --datadir.ancient=/ancient \
      --http --http.addr 0.0.0.0 --http.port 8545 \
      --ws --ws.addr 0.0.0.0 --ws.port 8546 \
-     --http.vhosts * --http.corsdomain *
+     --http.vhosts "*" --http.corsdomain "*"
