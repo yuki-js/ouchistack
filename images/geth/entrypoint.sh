167a753c3174100d6ace93abf021d316e156597e
yuki-js <20838151+yuki-js@users.noreply.github.com>
2022-03-03 12:07:22 +0000

    change ipc path

--- images/geth/entrypoint.sh
+++ images/geth/entrypoint.sh
@@ -2,4 +2,5 @@
 exec geth --datadir=/datadir --datadir.ancient=/ancient \
      --http --http.addr 0.0.0.0 --http.port 8545 \
      --ws --ws.addr 0.0.0.0 --ws.port 8546 \
+     --ipcpath /datadir/geth.ipc \
      --http.vhosts "*" --http.corsdomain "*"
