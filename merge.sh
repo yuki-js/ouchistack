03c076f954272fe65f11195ca3cac6beeb93c80b
yuki-js <20838151+yuki-js@users.noreply.github.com>
2022-03-03 16:14:22 +0000

    split files

---
+++ merge.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# set working directory to this script's directory
+cd "$(dirname "$0")"
+
+# usage
+usage() {
+  echo "Usage: STACKS=stack_list $0 [command/args]"
+  echo "  command/args: arguments to pass to compose"
+  echo "    will be up|down|restart|ps|etc."
+  exit 1
+}
+
+# the list of arguments to pass to docker compose
+ARGS=""
+STACKS=stacks/*.yml
+
+for STACK in $STACKS; do
+  ARGS="${ARGS} -f ${STACK}"
+done
+
+# run docker compose
+docker compose ${ARGS} config "$@"
