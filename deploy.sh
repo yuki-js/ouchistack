c81cb60f81ddd0015e5f72d96675159a44a4ff19
yuki-js <20838151+yuki-js@users.noreply.github.com>
2022-03-03 16:51:38 +0000

    rename

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,6 @@
 # set working directory to this script's directory
 cd "$(dirname "$0")"
 
-# usage
-usage() {
-  echo "Usage: STACKS=stack_list $0 [command/args]"
-  echo "  command/args: arguments to pass to compose"
-  echo "    will be up|down|restart|ps|etc."
-  exit 1
-}
-
 # the list of arguments to pass to docker compose
 ARGS=""
 STACKS=stacks/*.yml
